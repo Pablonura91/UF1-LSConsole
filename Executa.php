e7a6b3d1837ed1a42b97539ad6b581aacca51a80
Pablo Nuñez <pablonura91@gmail.com>
2019-02-26 12:48:45 +0100

    update executa

--- Executa.php
+++ Executa.php
@@ -3,14 +3,15 @@
 require_once './libs/directoris.inc';
 require_once './libs/arxius.inc';
 require_once './libs/sistema.inc';
+require_once './Constants.inc';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $command = $_POST["command"];
     $commandExplode = explode(" ", $command);
-    
-    if (substr($commandExplode[1], 0, 1) === '-') {
+
+    if (strpos($command, "-") && substr($commandExplode[1], 0, 1) === '-') {
         $iterator = 0;
-        $commandExplode[0] .= " ". $commandExplode[1];
+        $commandExplode[0] .= " " . $commandExplode[1];
         foreach ($commandExplode as $command) {
             if ($iterator > 1) {
                 $commandExplode[$iterator - 1] = $command;
@@ -20,48 +21,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     switch ($commandExplode[0]) {
-        case "mkdir":
-            if (create_directory($commandExplode[1])) {
-                echo "Creat correctament";
-            } else {
-                echo "Hi ha agut un error";
-            }
+        case COMMANDCREATEDIRECTORY:
+            create_directory($commandExplode[1]);
             break;
-        case "rm -d":
+        case COMMANDDELETEDIRECTORY:
             delete_directory($commandExplode[1]);
             break;
-        case "mv -d":
+        case COMMANDMOVEDIRECTORY:
             move_directory($commandExplode[1], $commandExplode[2]);
             break;
-        case "cp -d":
+        case COMMANDCOPYDIRECTORY:
             copy_directory($commandExplode[1], $commandExplode[2]);
             break;
-        case "find":
+        case COMMANDFINDFILE:
             find_file($commandExplode[1], $commandExplode[2]);
             break;
-        case "stats":
+        case COMMANDSTATSFILE:
             stats_file($commandExplode[1]);
             break;
-        case "rm -f":
+        case COMMANDDELETEFILE:
             delete_file($commandExplode[1]);
             break;
-        case "mv -f":
+        case COMMANDMOVEFILE:
             move_file($commandExplode[1], $commandExplode[2]);
             break;
-        case "cp -f":
+        case COMMANDCOPYFILE:
             copy_file($commandExplode[1], $commandExplode[2]);
             break;
-        case "sha1":
+        case COMMANDSHA1FILE:
             sha1_file_function($commandExplode[1]);
             break;
-        case "md5":
+        case COMMANDMD5FILE:
             md5_file_function($commandExplode[1]);
             break;
+        case COMMANDLSSYSTEM:
+            llistat(getcwd());
+            break;
+        case COMMANDDIRSYSTEM:
+            ruta();
+            break;
+        case COMMANDSTATSSYSTEMSYSTEM:
+            stats_sistema(getcwd());
+            break;
+        case COMMANDHELP:
+            commandExists();
+            break;
         default:
+            commandExists();
             break;
     }
 }
 
-function commandExists() {
-    return "mkdir directori</br> rm -d directori</br> mv -d  directori desti</br> cp -d directori desti </br>";
+function commandExists()
+{
+    foreach (help()['user'] as $command) {
+        echo $command . "<br>";
+    }
 }
